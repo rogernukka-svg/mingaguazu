2a996a466839e1b889c18d611548720aeba08c34
Tu Nombre <tu-email@ejemplo.com>
2026-03-14 08:42:27 -0300

    update: cambios recientes

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Jaha2045 from "./Jaha2045.jsx";
+import Jaha2041 from "./Jaha2041.jsx";
 import App from "./App.jsx";
 
 // 🔑 CSS GLOBAL DE LEAFLET (arregla la flecha del popup en producción)
@@ -38,10 +38,10 @@ function Main() {
     <Router>
       <Routes>
         {/* 🧠 Acceso principal */}
-        <Route path="/" element={<Jaha2045 onLogin={setUser} />} />
+        <Route path="/" element={<Jaha2041 onLogin={setUser} />} />
 
         {/* 📌 Fallback login */}
-        <Route path="/login" element={<Jaha2045 onLogin={setUser} />} />
+        <Route path="/login" element={<Jaha2041 onLogin={setUser} />} />
 
         {/* 🧩 App principal */}
         <Route
